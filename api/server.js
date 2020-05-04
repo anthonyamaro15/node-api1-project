3ba3668958e91276d35db848b1f147b7b94dd378
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-04 12:11:31 -0700

    added initial code

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const userRoute = require("../users/userRoute");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/users", userRoute);
+
+module.exports = server;

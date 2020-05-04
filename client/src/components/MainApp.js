ce6c3844ee4c748065fcab1e98fe15ecf01a0753
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-04 13:18:23 -0700

    added react-app

---
+++ client/src/components/MainApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useForms } from "react-hook-form";
+import axios from "axios";
+
+const MainApp = () => {
+  return (
+    <div className="MainApp">
+      <h2>MainApp</h2>
+    </div>
+  );
+};
+
+export default MainApp;
